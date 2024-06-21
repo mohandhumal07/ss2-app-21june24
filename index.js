4c4d4d4d27370fc2029e33b13085395105cdd876
mohandhumal07 <mohandhumal07@gmail.com>
2024-06-21 12:24:26 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const cors = require("cors");
+const mysql = require("mysql2");
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+let con = mysql.createConnection({
+					host : "sql12.freesqldatabase.com",
+					user : "sql12715119",
+					password : "9mKHb6Mlaf",
+					database : "sql12715119"
+		});
+	
+	app.post("/save", (req, res) => {
+				let data = [req.body.name,req.body.company,req.body.pkg];
+				let sql = "insert into student values(?, ?, ?)";
+				con.query(sql,data,(err, result) => {
+						if (err)					res.send(err);
+						else						res.send(result);
+				});
+	});
+
+
+app.listen(9000, () => { console.log("ready to serve @ 9000");});
